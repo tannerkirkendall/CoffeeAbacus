affb2ef22a8317c8a0c27b5583124212a7a888c8
Tanner Kirkendall <tkirkendall@trshealthcare.com>
2020-02-06 19:39:27 -0600

    init

---
+++ src/components/Calculator.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div>
+      <label>Total Brew:</label>
+      <input
+        id="inputTotal"
+        type="number"
+        placeholder
+        oninput="changeTotalBrew()"
+        onchange="changeTotalBrew()"
+      />
+      <select id="brewUnit" onchange="calculate()">
+        <option value="1">Milliliters</option>
+        <option value="236.588">Cups</option>
+        <option value="29.5735" selected>Ounces</option>
+      </select>
+    </div>
+    <div>
+      <label>Water:</label>
+      <input
+        id="inputWater"
+        type="number"
+        placeholder
+        oninput="changeWater()"
+        onchange="changeWater()"
+      />
+      <select id="waterUnit" onchange="calculate()">
+        <option value="1">Milliliters</option>
+        <option value="236.588">Cups</option>
+        <option value="29.5735">Ounces</option>
+      </select>
+    </div>
+    <div>
+      <label>Grounds:</label>
+      <input
+        id="inputCoffee"
+        type="number"
+        placeholder
+        oninput="changeGrounds()"
+        onchange="changeGrounds()"
+      />
+      <select id="groundsUnit" onchange="calculate()">
+        <option value="1">Grams</option>
+        <option value="28.3527076">Ounces</option>
+      </select>
+    </div>
+    <div>
+      <label>Ratio:</label>
+      <select id="inputRatio" name="inputRatio" onchange="calculate()">
+        <option value="14">14:1</option>
+        <option value="15">15:1</option>
+        <option value="16">16:1</option>
+        <option value="17" selected>17:1</option>
+        <option value="18">18:1</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Calculator />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Calculator from "@/components/Calculator.vue"
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Calculator
   }
 };
 </script>
