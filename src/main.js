6302f36729e18a20e4aaaadcd5a8dff456a99687
Tanner Kirkendall <tkirkendall@trshealthcare.com>
2020-03-01 12:28:02 -0600

    firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { id: "coffeeabacus-2020" }
+});
+
 
 Vue.config.productionTip = false;
 
