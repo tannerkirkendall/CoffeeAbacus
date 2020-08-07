134df25169410f0d646ece4b4c64443302d1bbba
Tanner Kirkendall <tkirkendall@trshealthcare.com>
2020-08-06 22:05:10 -0500

    added privacy policy

--- src/components/HowToUse.vue
+++ src/components/HowToUse.vue
@@ -1,7 +1,23 @@
 <template>
-    <p>Coffee Abacus is a calculator that help dial in the perfect cup of coffee. </p>    
+<div id="back-box">
+<v-container id="control-box" fluid>
+    <p>Coffee Abacus is a calculator that help dial in the perfect cup of coffee. </p>  
+</v-container>  
+</div>
 </template>
 
 <style scoped>
-
+.v-select {
+  font-size: 0.9em;
+}
+#back-box {
+  min-width: 200px;
+  background-color: whitesmoke;
+  border-radius: 15px;
+}
+#control-box {
+  padding: 30px;
+  /* width: 100%; */
+  /* margin: 0 auto; */
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <Calculator />
+
   </div>
 </template>
 
